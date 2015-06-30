08d3ef38ea4f208834b312757ce33feb18951a67
mayacode <maja.m.iarecki@gmail.com>
2015-06-30 14:22:31 +0200

    added first working version

--- custom.js
+++ custom.js
@@ -10,20 +10,62 @@ var addNewEventListener = function(element, eventName, foo){
 var d = document;
 var x = "X";
 var o = "O";
-var clickCounter = 0;
+var clickArray = [];
+var clicks = 0;
+
 var element = d.getElementById('game-board');
-var gameOver = function() {
+var gameOver = function(winner) {
     var div = d.getElementById('game-over');
     div.className="";
+    var text = "Game over!!!";
+    if (winner) {
+        text = "Game over!!! The winner is " + winner;
+    }
+    div.children[0].innerHTML = text;
+};
+var checkClickArray = function(){
+    var winList = [
+        [0, 4, 8],
+        [2, 4, 6],
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8]
+    ];
+    var ctx = [];
+    winList.some(function(el){
+        console.log(el, (clickArray[el[0]] === clickArray[el[1]]));
+        console.log(clickArray[el[0]],clickArray[el[1]],clickArray[el[2]]);
+        if (clickArray[el[0]] === clickArray[el[1]] && clickArray[el[1]] === clickArray[el[2]]) {
+            ctx.push(clickArray[el[0]]);
+            return true;
+        }
+    }, ctx);
+    return ctx;
 };
 var foo = function(e){
-    clickCounter++;
+    clicks ++;
     e.preventDefault();
     e.stopPropagation();
     var cell = e.target;
+    var index = cell.attributes[0].value * 1 - 1;
     var text = x;
-    if (clickCounter %2 == 0) { text = o; }
-    cell.innerHTML = text;
-    if (clickCounter === 9) { gameOver(); }
+    if (clicks %2 == 0) { text = o; }
+    if (!cell.innerHTML) {
+        cell.innerHTML = text;
+        clickArray[index] = text;
+    } else {
+        clicks --;
+    }
+    console.log(clickArray);
+    if (clicks > 4) {
+        var tmp = checkClickArray();
+        console.log(tmp);
+        if (tmp[0]) { gameOver(tmp[0]); }
+        if (clicks === 9) { gameOver(); }
+
+    }
 };
 addNewEventListener(element, 'click', foo);
